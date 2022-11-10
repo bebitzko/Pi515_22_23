3cdf16a43f524126ae9230da2af1ca12a71b9be7
Benjamin Bitzko <59434201+bebitzko@users.noreply.github.com>
2022-11-10 22:35:24 +0000

    Finished project 1

--- Project1/main.py
+++ Project1/main.py
@@ -2,31 +2,33 @@
 
 import random
 
-keepAsking = True
-while keepAsking:
-    num = input('Type a number for an upper bound: ')
-    if num.isdigit():
-        print("Let's play!")
-        num = int(num)
-        # something about the loop condition
+while True:
+    keepAsking     = True
+    while keepAsking:
+        num = input('Type a number for an upper bound: ')
+        if num.isdigit():
+            print("Let's play!")
+            num = int(num)
+            keepAsking = False
+        else:
+            print("Invalid input. Try again.")
+    
+    secret = random.randint(1, num)
+    guess = None
+    count = 0
+    
+    while guess != secret:
+        guess = input("Type a number between 1 and " + str(num) + ": ")
+        if guess.isdigit():
+            guess = int(guess)
+        if guess == secret: # can't always be wrong
+            print("You got it!")
+            count += 1
+        else:
+            print("Try again.")
+            count += 1
+    
+    if count == 1:
+        print("It took you", count, "guess!")
     else:
-        print("Invalid input. Try again.")
-
-secret = random.randint(1, num)
-guess = None
-count = 1
-
-while guess != secret:
-    guess = input("Type a number between 1 and " + str(num) + ": ")
-    if guess.isdigit():
-        guess = int(guess)
-    if False: # can't always be wrong
-        print("You got it!")
-    else:
-        print("Try again.")
-        # what else?
-
-if count == 1:
-    print("It took you", count, "guess!")
-else:
-    print("It took you", count, "guesses!")
+        print("It took you", count, "guesses!")
